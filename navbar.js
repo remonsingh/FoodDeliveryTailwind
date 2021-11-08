2fc05afc4c577a3ac19801bc6e3675ddd2551e32
remonsingh <76706263+remonsingh@users.noreply.github.com>
2021-11-08 17:02:47 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,14 @@
+function navbar(){
+    return `
+        <div><a href="./index.html">Home</a></div>
+        <div><a href="./menu.html">Menu</a></div>
+        <div>
+            <a href="./login.html">Login</a>
+        </div>
+        <div>
+            <a href="./signup.html">Signup</a>
+        </div>
+        <div><a href="./cart.html">Go to Cart</a></div>
+    `
+}
+export default navbar;
